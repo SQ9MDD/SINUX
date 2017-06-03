8e8bcb703f3090cdc00c8e48e6405e933660f01f
Rysiek Labus <rysieklabus@gmail.com>
2017-06-03 15:13:25 +0200

    Add files via upload

---
+++ sinux_as540/examples/push_on_push_off/push_on_push_off.ino
@@ -0,0 +1,22 @@
+// ******************* //
+// SINUX Ryszard Labus //
+// http://sinux.com.pl //
+// AS-540 firmware *** //
+// ******************* //
+
+#include <sinux_as540.h>
+
+sinux_as540 sterownik;
+
+void setup() {
+  sterownik.INIT(73);                                               // inicjalizacja i ustawianie adresu sieciowego
+}
+
+void loop() {
+  if(sterownik.BI_PRESSED(1)){  sterownik.BO_TOGGLE(1); }           // przycisk typu push on push off 
+  if(sterownik.BI_PRESSED(2)){  sterownik.BO_TOGGLE(2); }
+  if(sterownik.BI_PRESSED(3)){  sterownik.BO_TOGGLE(3); }
+  if(sterownik.BI_PRESSED(4)){  sterownik.BO_TOGGLE(4); }
+
+  sterownik.MAIN();                                                 // główny program biblioteki
+}
